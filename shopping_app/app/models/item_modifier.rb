3913af54be865d58340eff375e314edd7219e17c
Ridham Patel <ridham.patel@bacancy.com>
2023-03-11 01:04:01 +0530

    shopping app created for show active records association and query interface

---
+++ shopping_app/app/models/item_modifier.rb
@@ -0,0 +1,6 @@
+class ItemModifier < ApplicationRecord
+  belongs_to :item
+  belongs_to :modifier
+
+  default_scope -> { order(created_at: :asc) }
+end
---
+++ shopping_app/db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+# Create categories
+5.times do
+  Category.create(name: Faker::Commerce.department(max: 1))
+end
+
+# Create items
+10.times do
+  Item.create(name: Faker::Commerce.product_name, price: Faker::Commerce.price, stock_quantity: rand(0..100), category_id: Category.first.id, status: [true, false].sample)
+end
+
+# Create modifiers
+20.times do
+  Modifier.create(name: Faker::Commerce.product_name, price: Faker::Commerce.price, stock_quantity: rand(0..100))
+end
